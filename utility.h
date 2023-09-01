38ea8061a5b10efd82f70dee48c8bb472a4096c4
jiangwenzhuo <93459353+Beavercat7@users.noreply.github.com>
2023-09-01 21:20:25 +0800

    Update utility.h

--- utility.h
+++ utility.h
@@ -1,139 +1,114 @@
-#include "utility.h"
+#ifndef UTILITY_H_INCLUDED
+#define UTILITY_H_INCLUDED
 
-int main(int argc,char*argv[])
+#include <iostream>
+#include <list>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+list<int>clients_list;
+//保存客户端socket的列表
+
+#define SERVER_IP "127.0.0.1"
+//服务器端IP地址
+
+#define SERVER_PORT 8888
+//服务端端口号
+
+#define EPOLL_SIZE 5000
+//epoll 事件表的最大事件数
+
+#define BUF_SIZE 0xFFF
+//消息存储区的最大存储空间大小
+
+#define SERVER_WELCOME "Welcome you join to the chat room! Your chat ID is:Client #%d"
+
+#define SERVER_MESSAGE "ClientID %d say >> %s"
+
+//退出
+#define EXIT "EXIT"
+
+#define CAUTION "There is only one int the char room!"
+
+//fcntl()针对(文件)描述符提供控制,参数fd是被参数cmd操作(如下面的描述符)
+int setnonblocking(int sockfd)
 {
-    //用户连接的服务器 IP+port
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_family = PF_INET;
-    serverAddr.sin_port = htons(SERVER_PORT);
-    serverAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
-
-    //创建socket
-    int sock = socket(PF_INET,SOCK_STREAM,0);
-    if(sock < 0)
-    {
-        perror("connect error");
-        exit(-1);
-    }
-    //连接服务器
-    if(connect(sock,(struct sockaddr*)&serverAddr,sizeof(serverAddr))<0)
-    {
-        perror("connect error");
-        exit(-1);
-    }
-    
-    //创建管道,其中fd[0]用于父进程读,fd[1]用于子进程写
-    int pipe_fd[2];
-    if(pipe(pipe_fd)<0)
-    {
-        perror("pipe error");
-        exit(-1);
-    }
-    
-    //创建epoll
-    int epfd = epoll_create(EPOLL_SIZE);
-    if(epfd < 0)
-    {
-        perror("epfd error");
-        exit(-1);
-    }
-    static struct epoll_event events[2];
-    //将sock和管道读端描述符都添加到内核事件表
-    addFd(epfd,sock,true);
-    addFd(epfd,pipe_fd[0],true);
-    //表示客户端是否正常工作
-    bool isClientwork = true;
-
-    //聊天信息缓冲区
-    char message[BUF_SIZE];
-
-    //Fork
-    int pid = fork();
-    if(pid < 0)
-    {
-        perror("fork error");
-        exit(-1);
-    }
-    else if(pid == 0)//子进程
-    {
-        //子进程负责写入管道,因此先关闭读端
-        close(pipe_fd[0]);
-        printf("Please input 'exit' to exit the chat room!\n");
-
-        while(isClientwork)
-        {
-          bzero(&message,BUF_SIZE);
-          fgets(message,BUF_SIZE,stdin);
-
-          //客户端输出exit,退出
-          if(strncasecmp(message,EXIT,strlen(EXIT)))
-          isClientwork = 0;
-          else   //子进程将信息写入管道
-          {
-            if(write(pipe_fd[1],message,strlen(message) -1)<0)
-            {
-                perror("fork error");
-                exit(-1);
-            }
-          }
-        }
-        
-    }
-    else  //pid > 0 父进程
-    {
-       //父进程负责读管道数据,因此先关闭写端
-       close(pipe_fd[1]);
-
-       //主循环(epoll_wait)
-       while(isClientwork)
-       {
-         int epoll_events_count = epoll_wait(epfd,events,2,-1);
-         //处理就绪事件
-         for(int i=0;i<epoll_events_count;i++)
-         {
-            bzero(&message,BUF_SIZE);
-            
-            //服务器发来信息
-            if(events[i].data.fd == sock)
-            {
-                //接受服务端信息
-                int ret = recv(sock,message,BUF_SIZE,0);
-
-                //ret = 0 服务端关闭
-                if(ret == 0)
-                {
-                    printf("Server closed connnection: %d\n",sock);
-                    close(sock);
-                    isClientwork = 0;
-                }
-                else printf("%s\n",message);
-            }
-            //子进程写入事件发生,父进程处理并发送服务端
-            else 
-            {
-                //父进程从管道中读取数据
-                int ret = read(events[i].data.fd,message,BUF_SIZE);
-
-                //ret = 0;
-                if(ret == 0)isClientwork = 0;
-                else //将信息发送给服务器
-                {
-                   send(sock,message,BUF_SIZE,0);
-                }
-            }
-         }
-       }
-    }
-    if(pid)
-    {
-        //关闭父进程和sock
-        close(pipe_fd[0]);
-        close(sock);
-    }
-    else 
-    {
-        //关闭子进程
-        close(pipe_fd[1]);
-    }
+    fcntl(sockfd,F_SETFL,fcntl(sockfd,F_GETFD,0)|O_NONBLOCK);
+    //F_SETFL->设置fd的状态标志,但部分标志是不能被修改的(比如访问模式标志!)
+    //F_GETFD0>返回fd的相应文件标志
+    //总体意思就是:返回sockfd本来的文件描述符标志再|上O_NONBLOCK 最后用新的文件描述符标志来设置更新!
     return 0;
 }
+//设置socket为非阻塞模式
+
+void addFd(int epollfd,int fd,bool enable_et)
+{
+    struct epoll_event ev;
+    ev.data.fd = fd;
+    ev.events = EPOLLIN;
+    if(enable_et)
+    {
+        ev.events = EPOLLIN|EPOLLET;
+    }
+    epoll_ctl(epollfd,EPOLL_CTL_ADD,fd,&ev);
+    setnonblocking(fd);
+    printf("fd added to epoll!\n");
+}
+
+int sendBroadcastmessage(int clientfd)
+{
+    //buf[BUF_SIZE]接受新的聊天信息
+    //message[BUF_SIZE]保存格式信息
+    char buf[BUF_SIZE],message[BUF_SIZE];
+    bzero(buf,BUF_SIZE);
+    bzero(message,BUF_SIZE);
+
+    //接受信息
+    printf("read from client(clientID = %d)\n",clientfd);
+    int len = recv(clientfd,buf,BUF_SIZE,0);
+
+    if(len <= 0)//意味着客户端断开连接
+    {
+        close(clientfd);
+        clients_list.remove(clientfd); //服务器移除这个客户端
+        printf("ClientID = %d closed.\n now there are %d client in the chat room\n",clientfd,(int)clients_list.size());
+    }
+    else   //传播信息
+    {
+        if(clients_list.size()==1)//这意味着仅有一个在chat room
+        {
+          send(clientfd,CAUTION,strlen(CAUTION),0);
+          return len;
+        }
+        //format message to broadcast
+        sprintf(message,SERVER_MESSAGE,clientfd,buf);
+
+        list<int>::iterator it;
+        for(it = clients_list.begin();it!=clients_list.end();it++)
+        {
+            if(*it!=clientfd)
+            {
+                if(send(*it,message,BUF_SIZE,0)<0)
+                {
+                    perror("error");
+                    exit(-1);
+                }
+            }
+        }
+    }
+    return len;
+}
+#endif 
+
+
